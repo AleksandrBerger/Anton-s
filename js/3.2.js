0c3350fe5e2ecc5dbd5d78c6a129f0d69103873c
AleksandrBerger <44908572+AleksandrBerger@users.noreply.github.com>
2019-01-17 11:26:30 +0300

    123

---
+++ js/3.2.js
@@ -0,0 +1,11 @@
+let b = prompt("Введите размер доски");
+
+//process.stdin.on(b);
+for (let i=0; i<=b;i++) {
+    for (let j=0; j<=b; j++) {
+
+        //console.log ("#");
+        //process.stdout.write("# ");
+    }
+    process.stdout.write("\n");
+}
--- js/test.js
+++
@@ -1,6 +0,0 @@
-let a='abc';
-let b="def";
-let c=2;
-let d= '2';
-console.log(+d);
-console.log(c+a+b);
